823781c52be2a841eea58b2071afdfebb8f44160
Cheng® <cheng106.andy@gmail.com>
2021-06-26 00:12:01 +0800

    feat: jet stage init & adjust pom.xml

---
+++ Jet-Main/src/main/java/com/cheng/web/JetStageApplication.java
@@ -0,0 +1,13 @@
+package com.cheng.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class JetStageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JetStageApplication.class, args);
+        System.out.println("Service Start Success─=≡Σ((( つ•̀ω•́)つ");
+    }
+}
